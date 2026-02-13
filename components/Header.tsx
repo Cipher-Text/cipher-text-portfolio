481b8c7ff3e74de3c13888ce709907b7c4801894
tajmin <tajminur.aiub@gmail.com>
2026-02-13 21:25:40 +0600

    logo update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { getSiteConfig } from "@/lib/content";
@@ -14,7 +15,7 @@ export default function Header() {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <Link href="/" className="text-xl font-bold text-slate-900">
-            {site.name}
+            <Image src="/logo.svg" alt={site.name} width={200} height={60} />
           </Link>
 
           {/* Desktop Navigation */}
